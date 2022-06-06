25b7b41eeab2b867b908ef2bb9cb275b61ac3fe6
villocan <cesar.villocan@gmail.com>
2022-06-06 18:39:53 +0100

    m

--- Problem_0002.py
+++ Problem_0002.py
@@ -32,5 +32,3 @@ def solution():
     return total
 
 print(solution())
-
-
---
+++ Problem_0003.py
@@ -0,0 +1,36 @@
+'''
+Problem ID:
+0003
+
+Problem Title:
+Largest prime factor
+
+Problem Description:
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+
+Problem Solution:
+6857
+'''
+
+def isPrime(N):
+    multiples=0
+    for n in range(1,N):
+        if N%(N-n)==0:
+            multiples+=1
+        if multiples>1:
+            return False
+    return True
+
+def solution(N):
+    total=1    #
+    factor=0   #
+    while total<N:
+        factor+=1
+        while not(isPrime(factor)):
+            factor+=1
+        if N%factor==0:
+            total*=factor
+    return factor
+
+print(solution(600851475143))

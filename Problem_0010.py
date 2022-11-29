009cff2941c8bec6e1790abf39b3f6423161ceb6
villocan <cesar.villocan@gmail.com>
2022-11-29 23:54:22 +0000

    Update Problem_0010.py

--- Problem_0010.py
+++ Problem_0010.py
@@ -13,25 +13,14 @@ Problem Solution:
 
 '''
 import time
-
-def isPrime(number):
-    result = True
-    divisor = 2
-    while divisor != number:        
-        if number%divisor == 0:
-            result = False
-            break
-        divisor += 1
-    return result
+from primality import primality as prim
 
 def solution(number):
     result = 0
-    for k in range(2,number):
-        print(k)
-        if isPrime(k):
-            result+=k
+    for prime in prim.prange(number):
+        result += prime
     return result
 
 timer=time.time()
-print(solution(10))
+print(solution(2*10**6))
 print(f'It took {round(time.time()-timer,4)} secs')

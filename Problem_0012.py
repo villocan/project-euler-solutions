337986b4bf6c169c45c87e92c5545c49807b428c
villocan <cesar.villocan@gmail.com>
2022-12-01 22:25:14 +0000

    Create Problem_0012.py

---
+++ Problem_0012.py
@@ -0,0 +1,22 @@
+'''
+Problem ID:
+PE0012
+
+Problem Title:
+
+
+Problem Description:
+
+
+Problem Solution:
+
+'''
+import time
+
+def solution():
+    result = 0
+    return result
+
+timer=time.time()
+print(solution())
+print(f'It took {round(time.time()-timer,4)} secs')

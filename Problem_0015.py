75fff3636825391e0ad8e2cd824e3a867658dd41
villocan <cesar.villocan@gmail.com>
2022-12-05 20:40:51 +0000

    Create Problem_0015.py

---
+++ Problem_0015.py
@@ -0,0 +1,23 @@
+'''
+Problem ID:
+PE0015
+
+Problem Title:
+Lattice paths
+
+Problem Description:
+Starting in the top left corner of a 2*2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 20*20 grid?
+
+Problem Solution:
+
+'''
+import time
+
+def solution():
+    result = 0
+    return result
+
+timer=time.time()
+print(solution())
+print(f'It took {round(time.time()-timer,4)} secs')
